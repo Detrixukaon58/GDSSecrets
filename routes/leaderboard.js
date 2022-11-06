50f65df84141d2c708598bf376b2cc2b062733b3
Havandeep Singh Khatkar <hxs053@cs.bham.ac.uk>
2022-11-06 16:01:24 +0000

    got rid of row being printed in leaderboard.js

--- routes/leaderboard.js
+++ routes/leaderboard.js
@@ -6,12 +6,10 @@ router.get('/', function(req, res, next) {
     db.serialize(() => {
         db.each('SELECT name, score FROM player', (err, row) =>{
             if(err){
-                console.error(err.message);
                 req.flash('error', err);
                 res.render('leaderboard', {data: ''});
             }
             else{
-                console.log(row);
                 res.render('leaderboard', {data: [row]});
             }
         })
